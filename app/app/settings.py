b377939aa9b84658e3effa85e059819bec307602
River <riveriswild@icloud.com>
2022-04-19 11:58:01 +0300

    Initial commit

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--^bwun^ps^yq498h+u2=4fol5cgzq@@!u0kle^*e99jiuno%_#'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
